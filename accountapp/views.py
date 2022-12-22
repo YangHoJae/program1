8b0021f24df7d7a6aac2ebb02b5f7dbfc2de279a
양호재 <didghwo12@naver.com>
2022-12-22 16:16:53 +0900

    django 17 course commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -7,7 +7,6 @@ from accountapp.models import HelloWorld
 # Create your views here.
 
 def hello_world(request):
-
     if request.method == "POST":
 
         temp = request.POST.get('hello_world_input')
@@ -16,8 +15,6 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': new_hello_world })
+        return render(request, 'accountapp/hello_world.html', context=  {'hello_world_output': new_hello_world})
     else:
         return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
-
