906d64450d801a58778807a44853c9c7e5eae568
양호재 <ghwo2383@naver.com>
2023-01-03 08:28:33 +0900

    django course 29 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,7 +6,6 @@ from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
-
 from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
@@ -50,7 +49,7 @@ class AccountUpdateView(UpdateView):
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
 
-@method_decorator(has_ownership, 'get') 
+@method_decorator(has_ownership, 'get')
 @method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
